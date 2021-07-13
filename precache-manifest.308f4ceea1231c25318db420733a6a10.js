c8205ca77e97997a99860ff6afa51d06454f7b2e
Bestian Tang <bestian@gmail.com>
2021-07-13 14:11:46 +0800

    deploy

---
+++ precache-manifest.308f4ceea1231c25318db420733a6a10.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3455d756e0c8284d83834384403fda68",
+    "url": "/ad-be.jpg"
+  },
+  {
+    "revision": "e27b8103e821cf76fa85",
+    "url": "/css/app.b42c83be.css"
+  },
+  {
+    "revision": "182207207254c3531143",
+    "url": "/css/chunk-vendors.e1921c98.css"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "38ad27eb9467e96a56a0eaafccaf77d8",
+    "url": "/img/logo.38ad27eb.png"
+  },
+  {
+    "revision": "c7ae5afe5b64db38d536a67e67374fad",
+    "url": "/img/mathquiz.c7ae5afe.jpg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "2a1b4f933515d1b858f3cb967936fc28",
+    "url": "/img/quiz.2a1b4f93.png"
+  },
+  {
+    "revision": "ccb2778048ac903fde2dc41a456dba11",
+    "url": "/img/th.ccb27780.jpg"
+  },
+  {
+    "revision": "6c462b003605a1c7a045c33778fe85a0",
+    "url": "/img/摸石頭.6c462b00.png"
+  },
+  {
+    "revision": "2d2c95aad49bc9d0504a3e64a8c20be0",
+    "url": "/img/教材列表.2d2c95aa.png"
+  },
+  {
+    "revision": "ff5aea8c969ba5452c092a5504de9e36",
+    "url": "/img/方格紙產生器.ff5aea8c.png"
+  },
+  {
+    "revision": "00703c480d5128acafd1452196ade755",
+    "url": "/img/湊12.00703c48.png"
+  },
+  {
+    "revision": "c5c7c1c953bdbcc76728fa1ca1eb736f",
+    "url": "/img/閃卡.c5c7c1c9.png"
+  },
+  {
+    "revision": "0f7ca5b633d0b36d1b21bd9749a56dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27b8103e821cf76fa85",
+    "url": "/js/app.b6fe5add.js"
+  },
+  {
+    "revision": "182207207254c3531143",
+    "url": "/js/chunk-vendors.60959f3c.js"
+  },
+  {
+    "revision": "04524fd0bb613d155ccbdb68ad2c9e9c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/fonts/brand-icons.13db00b.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/fonts/brand-icons.a046592.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/fonts/brand-icons.c5ebe0b.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/fonts/brand-icons.e8c322d.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/fonts/icons.0ab5415.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/fonts/icons.8e3c7f5.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/fonts/icons.b87b9ba.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/fonts/icons.faff921.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/fonts/outline-icons.701ae6a.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/fonts/outline-icons.ad97afd.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/fonts/outline-icons.cd6c777.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/fonts/outline-icons.ef60a4f.woff"
+  },
+  {
+    "revision": "241b5ba005aa0a3c7852847b0b1390c4",
+    "url": "/static/img/Bestian001.jpg"
+  },
+  {
+    "revision": "540a0a5d9357abe7f40de44bb1eb104f",
+    "url": "/static/img/Chiajen001.jpg"
+  },
+  {
+    "revision": "356698dc921bce5f27e98a1ff95c48ad",
+    "url": "/static/img/bg-img.jpeg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/img/brand-icons.a1a749e.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/img/flags.9c74e17.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/img/icons.962a1bf.svg"
+  },
+  {
+    "revision": "38ad27eb9467e96a56a0eaafccaf77d8",
+    "url": "/static/img/logo.38ad27e.png"
+  },
+  {
+    "revision": "38ad27eb9467e96a56a0eaafccaf77d8",
+    "url": "/static/img/math-logo-big.png"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/img/outline-icons.82f60bd.svg"
+  },
+  {
+    "revision": "6c462b003605a1c7a045c33778fe85a0",
+    "url": "/static/img/摸石頭.6c462b0.png"
+  },
+  {
+    "revision": "2d2c95aad49bc9d0504a3e64a8c20be0",
+    "url": "/static/img/教材列表.2d2c95a.png"
+  },
+  {
+    "revision": "ff5aea8c969ba5452c092a5504de9e36",
+    "url": "/static/img/方格紙產生器.ff5aea8.png"
+  },
+  {
+    "revision": "00703c480d5128acafd1452196ade755",
+    "url": "/static/img/湊12.00703c4.png"
+  },
+  {
+    "revision": "486d04c93aa292eb867fd5b3f7edd74a",
+    "url": "/static/manifest.json"
+  },
+  {
+    "revision": "5d21c15cb17cd1fc56dacfac0de13fbf",
+    "url": "/static/maps/國中數學地圖.pdf"
+  },
+  {
+    "revision": "4f499351092675e0101716454dd88056",
+    "url": "/static/maps/國中數學地圖/p0.jpg"
+  },
+  {
+    "revision": "603e42334421a9ff53a8904c4072f553",
+    "url": "/static/maps/國中數學地圖/p1.jpg"
+  },
+  {
+    "revision": "79bbf29c81288d9d30f70b6b18b8867f",
+    "url": "/static/maps/國中數學地圖/p2.jpg"
+  },
+  {
+    "revision": "3f06b6d64f06434ac2fd3b5bc9ce634a",
+    "url": "/static/maps/國中數學地圖/p3.jpg"
+  },
+  {
+    "revision": "882b8f222a2f93fd666360d48288a592",
+    "url": "/static/maps/國中數學地圖/p4.jpg"
+  },
+  {
+    "revision": "1816b5822bcb05704b42127f34d540d0",
+    "url": "/static/maps/國中數學地圖/p5.jpg"
+  },
+  {
+    "revision": "0844c2b14c799727ed697b0c80d4b039",
+    "url": "/static/maps/小學數學地圖.pdf"
+  },
+  {
+    "revision": "d5d4b1e49d34151cea5eec639888bd1b",
+    "url": "/static/maps/小學數學地圖/p0.jpg"
+  },
+  {
+    "revision": "1c3dcfc4f03e93dd97903669ac02cdfe",
+    "url": "/static/maps/小學數學地圖/p1.jpg"
+  },
+  {
+    "revision": "833d2310f65d739c128cdad369447fa0",
+    "url": "/static/maps/小學數學地圖/p2.jpg"
+  },
+  {
+    "revision": "c781371e882e9f4f872aa1abb39088d4",
+    "url": "/static/maps/小學數學地圖/p3.jpg"
+  },
+  {
+    "revision": "de877904a2118fe344935e124fd117e5",
+    "url": "/static/maps/小學數學地圖/p4.jpg"
+  },
+  {
+    "revision": "78b4b6baaa9e32dd24b1585673548d3f",
+    "url": "/static/maps/小學數學地圖/p5.jpg"
+  },
+  {
+    "revision": "0af654345394586b90ad0ab886790e12",
+    "url": "/static/maps/高中數學地圖.pdf"
+  },
+  {
+    "revision": "55c39e69311e23a3db8b27f3f326d694",
+    "url": "/static/maps/高中數學地圖/p0.jpg"
+  },
+  {
+    "revision": "be757cd0808e334ff333ffc75fde6c05",
+    "url": "/static/maps/高中數學地圖/p1.jpg"
+  },
+  {
+    "revision": "ae2be349dbcdc9a89cc2282d60e93e2a",
+    "url": "/static/maps/高中數學地圖/p2.jpg"
+  },
+  {
+    "revision": "a109b7d56e4dd22a047f4c469ee49dc9",
+    "url": "/static/maps/高中數學地圖/p3.jpg"
+  },
+  {
+    "revision": "59685413b1402a9fda7f6025b51515d3",
+    "url": "/static/maps/高中數學地圖/p4.jpg"
+  },
+  {
+    "revision": "1816b5822bcb05704b42127f34d540d0",
+    "url": "/static/maps/高中數學地圖/p5.jpg"
+  },
+  {
+    "revision": "efe6c55bba6d2e3c5c94820a88ad1d81",
+    "url": "/static/quizs.json"
+  },
+  {
+    "revision": "fc9ed759e2517e5b742aeb77f932bd91",
+    "url": "/static/一元一次方程式.md"
+  },
+  {
+    "revision": "0459507d267b9acbe210f9afafe7c0f3",
+    "url": "/static/乘法.md"
+  },
+  {
+    "revision": "4eeab7eda46b5b5fa575767daebabf87",
+    "url": "/static/加法.md"
+  },
+  {
+    "revision": "b953bfaae1f68edab63d1f6109f51edb",
+    "url": "/static/圓與角.md"
+  },
+  {
+    "revision": "e04d3d2a851e7808032246506cc25689",
+    "url": "/static/正負數.md"
+  },
+  {
+    "revision": "720b62431f83292f24d71443e20a484d",
+    "url": "/static/減法.md"
+  },
+  {
+    "revision": "729b1452ac0a2a2622a3c85cfd6a15eb",
+    "url": "/static/除法.md"
+  },
+  {
+    "revision": "e23f2e4594eef574f26bf892aa39596d",
+    "url": "/static/黃金比例.jpg"
+  }
+]);
